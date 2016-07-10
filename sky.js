e9a9d12e3308b7e4473f0feaa6e1cbd74268b508
AndrewKralovec <akrala@siu.edu>
2016-07-10 00:10:45 -0500

    easier to read tagging

--- sky.js
+++ sky.js
@@ -1,26 +1,35 @@
 const fs = require('fs'); // this engine requires the fs module
 const readline = require('readline');
 
-module.exports = function (filePath) {
-var next='', buffer='', tag=false;
+module.exports = function (filePath,callback) {
     const readable = fs.createReadStream(filePath, {
         encoding: 'utf8',
         fd: null,
     });
 
     readable.on('readable', function() {
+    var next='', block='', buffer='', stack=[];
         while (null !== (next = readable.read(1))) {
             if(next == '#'){
-                if(tag){
-                    buffer+='>';
-                }else{
-                    buffer+='<';
+                // Read in the html tag block
+                while(null !== (next = readable.read(1))){
+                    if (!next.match(/[a-z]/i))
+                        break ; 
+                    block+=next; 
                 }
-                tag = !tag ; 
+                var temp = "<"+block+">" ;
+                // Check if opening or closing # tage 
+                if(temp == '<>'){
+                    buffer += stack.pop(); 
+                }else{
+                    buffer += temp ;  
+                    stack.push("</"+block+">"); 
+                }
+                block=''; 
             }else{
                 buffer += next; 
             }
         }
-        console.log(buffer); 
+        callback(null,buffer); 
     });
 };
