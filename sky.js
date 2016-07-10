fa3389777d1a17793db7c05f3e3bd51bc9bcf47c
AndrewKralovec <akrala@siu.edu>
2016-07-10 12:58:43 -0500

    match attributes

--- sky.js
+++ sky.js
@@ -10,10 +10,15 @@ module.exports = function (filePath,callback) {
     readable.on('readable', function() {
     var next='', block='', buffer='', stack=[];
         while (null !== (next = readable.read(1))) {
+            // html tag 
             if(next == '#'){
                 // Read in the html tag block
                 while(null !== (next = readable.read(1))){
-                    if (!next.match(/[a-z]/i))
+                    if(next ==='(' || next === ')'){
+                        block+=' '; 
+                        continue ; 
+                    }
+                    if (!next.match(/[a-z]/i) && !next.match(/["'=]/gi))
                         break ; 
                     block+=next; 
                 }
@@ -26,7 +31,19 @@ module.exports = function (filePath,callback) {
                     stack.push("</"+block+">"); 
                 }
                 block=''; 
-            }else{
+            }
+            // Extends tag
+            else if(next == '@'){
+                var file =''; 
+                while(null !== (next = readable.read(1))){
+                    if (!next.match(/[a-z]/i))
+                        break ; 
+                    file+=next; 
+                }
+                // Load that file rendered text 
+            }
+            // Content
+            else{
                 buffer += next; 
             }
         }
