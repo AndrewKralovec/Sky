88039d063bbb57b253fc642522000ce87a77ac4e
AndrewKralovec <akrala@siu.edu>
2016-07-11 21:24:53 -0500

    ES6 cross over

--- sky.js
+++ sky.js
@@ -1,40 +1,47 @@
-const fs = require('fs'); // this engine requires the fs module
-const readline = require('readline');
-
-module.exports = function (filePath,options,callback) {
-    var stack=[];
-    
-    const add = function(block,skip){
+var fs = require('fs'); // this engine requires the fs module
+var contents = null ; 
+var size = 0 ; 
+var stack=[];
+var options = null ; 
+class Sky {
+    constructor(filePath,options) {
+        this.contents = fs.readFileSync(filePath, 'utf-8');
+        this.size = this.contents.length ; 
+        this.options = options ; 
+        this.stack = [];  
+    }
+    getSize(){
+       return this.size ;  
+    }
+    malloc(value){
+        this.size+=value ;  
+    }
+    getProp(parm){
+        return (this.options[parm]); 
+    }
+    add(block,skip){
         if(skip)
             return block ; 
         var temp = "<"+block+">" ;
         var stemp = temp.substring(0, temp.indexOf(' ')); 
         // Check if opening or closing # tage 
         if(temp == '<>'){
-            return (stack.pop()); 
+            return (this.stack.pop()); 
         }else if(stemp === ''){
-            stack.push("</"+block+">"); 
+            this.stack.push("</"+block+">"); 
             return (temp); 
         }else{
-            stack.push(stemp.replace('<','</')+">");
+            this.stack.push(stemp.replace('<','</')+">");
             return (temp); 
         }
-    }; 
-    const getProp = function(parm){
-        return (options[parm]); 
-    }; 
-    const readable = fs.createReadStream(filePath, {
-        encoding: 'utf8',
-        fd: null,
-    });
-
-    readable.on('readable', function() {
-        var next='', block='',buffer='',skip=false; 
-        while (null !== (next = readable.read(1))) {
+    }
+    render(){
+        var next='',count=0, block='', buffer='', skip=false ;
+        while (null !== (next = this.contents[count++]) && count < this.getSize() && next !== undefined) {
             // html tag 
             if(next == '#'){
                 // Read in the html tag block
-                while(null !== (next = readable.read(1))){
+                while(null !== (next = this.contents[count++])){
                     if(next ==='(' || next === ')'){
                         block+=' '; 
                         continue ; 
@@ -42,26 +49,26 @@ module.exports = function (filePath,options,callback) {
                     if(next ==='{'){
                         skip = true ; 
                         var obj=''; 
-                        while(null !== (next = readable.read(1))){
+                        while(null !== (next = this.contents[count++])){
                             if(next === '}')
                                 break ; 
                             obj+=next; 
                         }
-                        block+=getProp(obj); 
+                        block+=this.getProp(obj); 
                         continue ; 
                     }
-                    if (!next.match(/[a-z]/i) && !next.match(/["'=]/gi))
+                    if (!next.match(/[a-z]/i) && !next.match(/["'=]/gi) && !next.match(/^\d+$/))
                         break ; 
                     block+=next; 
                 }
-                buffer += add(block,skip); 
+                buffer += this.add(block,skip); 
                 block=''; 
                 skip=false; 
             }
             // Extends tag
             else if(next == '@'){
                 var file =''; 
-                while(null !== (next = readable.read(1))){
+                while(null !== (next = this.contents[count++])){
                     if (!next.match(/[a-z]/i))
                         break ; 
                     file+=next; 
@@ -73,6 +80,10 @@ module.exports = function (filePath,options,callback) {
                 buffer += next; 
             }
         }
-        callback(null,buffer); 
-    });
-};
+        return(buffer); 
+    }
+    
+    
+}
+
+module.exports = Sky ; 
